0dff3fcad88ba5bcbfb3641fff7e52ef6d7241f0
saneesh <sani9809@gmail.com>
2026-09-01 19:51:46 +0400

    Add cPanel PORT support in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || process.env.SERVER_PORT || 5000;
 
 // Middleware
 app.use(cors());
